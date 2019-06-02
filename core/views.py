17f302266ee105d7cececbc213fd0848bf281ce6
Patricio Muñoz <patriciomunozroa@outlook.com>
2019-06-02 19:24:43 -0400

    Ultimo update

--- core/views.py
+++ core/views.py
@@ -6,3 +6,6 @@ from django.views.generic import TemplateView
 
 class HomeView(TemplateView):
     template_name = "core/home.html"
+
+class AboutView(TemplateView):
+    template_name = "core/about.html"
